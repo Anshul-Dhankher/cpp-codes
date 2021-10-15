904dcf80d324467249719de6fefa7a6a63cea746
iamthebugyouneed <54017422+iamthebugyouneed@users.noreply.github.com>
2021-10-15 22:07:50 +0530

    Add files via upload

---
+++ cf/5.cpp
@@ -0,0 +1,35 @@
+/**
+ *    Author:ANSHUL DHANKHER
+ *     YMCA CE-2023
+ *    KEEP HUSTLING!!!
+**/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+#define MOD 1000000007
+#define INF INT_MAX
+using namespace std;
+
+void solve()
+{}
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+		solve();
+
+	return 0;
+
+}
